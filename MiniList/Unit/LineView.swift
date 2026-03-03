cebb61006ac330931a7dc595ceb34214f29ddb08
Anton Cherkasov <unsaferawpointer@gmail.com>
2026-03-03 20:22:11 +0300

    [mvp]: Refresh text field data when focus is lost or when Enter is pressed

--- MiniList/Unit/LineView.swift
+++ MiniList/Unit/LineView.swift
@@ -8,21 +8,52 @@
 import SwiftUI
 
 struct LineView {
-	
+
 	@Binding var line: Line
+
+	// MARK: - Local State
+
+	@FocusState private var focus: Bool
+
+	@State private var text: String
+
+	// MARK: - Initialization
+
+	init(line: Binding<Line>) {
+		self._line = line
+		self._text = State(initialValue: line.wrappedValue.text)
+	}
 }
 
 // MARK: - View
 extension LineView: View {
-	
+
 	var body: some View {
 		HStack {
 			Circle()
 				.foregroundStyle(.tertiary)
 				.frame(width: 4, height: 4)
-			TextField("Required", text: $line.text)
+			TextField("Required", text: $text)
 				.foregroundStyle(line.isCompleted ? .tertiary : .primary)
+				.focused($focus)
+				.onSubmit {
+					onChange(newValue: text)
+				}
+				.onChange(of: focus) {
+					onChange(newValue: text)
+				}
+				.onChange(of: line.text) { oldValue, newValue in
+					text = newValue
+				}
 		}
+	}
+}
+
+// MARK: - Helpers
+private extension LineView {
+
+	func onChange(newValue: String) {
+		line.text = newValue
 	}
 }
 
